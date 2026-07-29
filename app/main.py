059340b08e5b3ecfa4aa141d29f035f32bc67c57
Siddhesh Shinde <siddheshshinde@Siddheshs-MacBook-Air.local>
2026-07-29 21:52:07 +0530

    added netflic cors

--- app/main.py
+++ app/main.py
@@ -32,7 +32,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://cortex-client.onrender.com"
+        "https://cortex-client.onrender.com",
+        "https://cortex-client.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
